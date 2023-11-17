9dcc5f1dbc9496892fa6de098b992c459f5b494a
Anastasiia Tverdostup <anastasiia.tverdostup@zfx.com>
2023-11-17 19:19:22 +0400

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
 import { BlockRenderer } from "components/BlockRenderer";
 import { MainMenu } from "components/MainMenu";
-import { getPageStaticProps } from "utils/getPageStaticProps";
+// import { getPageStaticProps } from "utils/getPageStaticProps";
 import { Page } from "components/Page";
 
-export default Page;
+// export default Page;
 
-export const getStaticProps = getPageStaticProps;
+// export const getStaticProps = getPageStaticProps;
+
+export default function Home(props) {
+  return <div>This is home page</div>;
+}
+
+export const getPageStaticProps = async () => {
+  return {
+    props: {
+      exampleProps: "example property",
+    },
+  };
+};
